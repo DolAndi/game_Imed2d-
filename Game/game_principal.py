39eb20e986488ac58f3d8d10700811cc3c006f78
Andriano <anditoazza@gmail.com>
2021-11-17 10:14:41 -0300

    adicao e edicao de arquivos
    Adição da pasta assets2 e arquivos de imagens e sons para o game e algumas mudanças no código

--- Game/game_principal.py
+++ Game/game_principal.py
@@ -2,27 +2,28 @@ import pygame
 import time
 import random
 pygame.init()
-largura = 800
-altura = 600
+largura = 736
+altura = 669
 configTela = (largura, altura)
 gameDisplay = pygame.display.set_mode(configTela)
 clock = pygame.time.Clock()
 black = (0, 0, 0)
 white = (255, 255, 255)
-pygame.display.set_caption("IronMan - Marcão")
-icone = pygame.image.load("assets/ironIcon.png")
+pygame.display.set_caption("Pokémon Dodge")
+icone = pygame.image.load("assets2/pikachuicon.png")
 pygame.display.set_icon(icone)
-ironMan = pygame.image.load("assets/ironLarge.png")
-larguraIronMan = 110
-fundo = pygame.image.load("assets/sky.png")
-missel = pygame.image.load("assets/missile.png")
-explosaoSound = pygame.mixer.Sound("assets/explosao.wav")
-misselSound = pygame.mixer.Sound("assets/missile.wav")
-misselSound.set_volume(0.2)
-def mostraIron(x, y):
-    gameDisplay.blit(ironMan, (x, y))
-def mostraMissel(x, y):
-    gameDisplay.blit(missel, (x, y))
+pikachu = pygame.image.load("assets2/pikachu2.png")
+larguraPikachu = 110
+fundo = pygame.image.load("assets2/cidade_pallet.png")
+pokeball = pygame.image.load("assets2/pokeball2.jpg")
+capturadoSound = pygame.mixer.Sound("assets2/pikachu_scream.mp3")
+descargaSound = pygame.mixer.Sound("assets2/descarga.mp3")
+descargaSound.set_volume(0.2)
+
+def mostraPikachu(x, y):
+    gameDisplay.blit(pikachu, (x, y))
+def mostraPokeball(x, y):
+    gameDisplay.blit(pokeball, (x, y))
 def text_objects(texto, font):
     textSurface = font.render(texto, True, black)
     return textSurface, textSurface.get_rect()
@@ -39,24 +40,25 @@ def escreverPlacar(contador):
     texto = fonte.render("Desvios:"+str(contador), True, white)
     gameDisplay.blit(texto, (10, 10))
 def dead():
-    pygame.mixer.Sound.play(explosaoSound)
+    pygame.mixer.Sound.play(capturadoSound)
     pygame.mixer.music.stop()
     escreverTela("Você Morreu!")
+
 def game():
-    pygame.mixer.music.load("assets/ironsound.mp3")
-    pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.music.load("assets2/pokemonbattle.mp3")
+    pygame.mixer.music.set_volume(0.3)
     pygame.mixer.music.play(-1)
-    ironPosicaoX = largura*0.42
-    ironPosicaoY = altura*0.8
+    ironPosicaoX = largura*0.2
+    ironPosicaoY = altura*0.2
     movimentoX = 0
-    velocidade = 20
-    misselAltura = 250
-    misselLargura = 50
+    velocidade = 15
+    misselAltura = 25
+    misselLargura = 25
     misselVelocidade = 3
     misselX = random.randrange(0, largura)
     misselY = -200
     desvios = 0
-    pygame.mixer.Sound.play(misselSound)
+    pygame.mixer.Sound.play(descargaSound)
     while True:
         # pega as ações da tela. Ex.: fechar, click de uma tecla ou do mouse
         acoes = pygame.event.get()  # devolve uma lista de ações
@@ -79,24 +81,24 @@ def game():
         # definindo o fundo do game]
         escreverPlacar(desvios)
         misselY = misselY + misselVelocidade
-        mostraMissel(misselX, misselY)
+        mostraPokeball(misselX, misselY)
         if misselY > altura:
             misselY = -200
             misselX = random.randrange(0, largura)
             desvios = desvios+1
             misselVelocidade += 3
-            pygame.mixer.Sound.play(misselSound)
+            pygame.mixer.Sound.play(descargaSound)
         ironPosicaoX += movimentoX
         if ironPosicaoX < 0:
             ironPosicaoX = 0
-        elif ironPosicaoX > largura-larguraIronMan:
-            ironPosicaoX = largura-larguraIronMan
+        elif ironPosicaoX > largura-larguraPikachu:
+            ironPosicaoX = largura-larguraPikachu
         # analise de colisão com o IronMan
         if ironPosicaoY < misselY + misselAltura:
-            if ironPosicaoX < misselX and ironPosicaoX+larguraIronMan > misselX or misselX+misselLargura > ironPosicaoX and misselX+misselLargura < ironPosicaoX+larguraIronMan:
+            if ironPosicaoX < misselX and ironPosicaoX+larguraPikachu > misselX or misselX+misselLargura > ironPosicaoX and misselX+misselLargura < ironPosicaoX+larguraPikachu:
                 dead()
         # analise de colisão com o IronMan
-        mostraIron(ironPosicaoX, ironPosicaoY)
+        mostraPikachu(ironPosicaoX, ironPosicaoY)
         pygame.display.update()
         clock.tick(60)  # faz com que o while execute 60x por segundo
 game()
--- Game/setup.py
+++ Game/setup.py
@@ -1,6 +1,6 @@
 import cx_Freeze
 executables = [cx_Freeze.Executable(
-    script="jogo.py", icon="assets/ironIcon.ico")]
+    script="game_principal.py", icon="assets/ironIcon.ico")]
 
 cx_Freeze.setup(
     name="Iron Man Dead",
